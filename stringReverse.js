de544afc84f2eb33be1e12de4773f2980b3860f3
Ketan <ketantoo@gmail.com>
2020-01-20 15:54:01 +0530

    string reverse solution

--- stringReverse.js
+++ stringReverse.js
@@ -4,3 +4,11 @@ const reverseString = str => {
     .reverse()
     .join("");
 };
+
+const reverseString2 = str => {
+  let sol = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    sol += str[i];
+  }
+  return sol;
+};
