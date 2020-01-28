448f5796ea5bef0bdc33de6dfca26f2d4d2b052c
Ketan <ketantoo@gmail.com>
2020-01-28 21:35:55 +0530

    Linked list with append function

---
+++ Linked List/LinkedList.js
@@ -0,0 +1,34 @@
+// let myLinkedList = {
+//   head: {
+//     value: 10,
+//     next: {
+//       value: 5,
+//       next: {
+//         value: 16,
+//         next: null
+//       }
+//     }
+//   }
+// };
+
+class LinkedList {
+  constructor(value, next) {
+    this.head = {
+      value: value,
+      next: null
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  append(value) {
+    // this.tail.next = { value: value, next: null };
+    // this.tail = this.tail.next;
+    // this.length++;
+    const newNode = { value: value, next: null };
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+}
